73a4eedbbdac1ce2668fd80b504a5ec602297c38
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-12-21 17:39:13 -0600

    WIP

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -12,7 +12,7 @@ class Install extends Command
 
     public function handle()
     {
-        $this->call('publish', ['--provider' => 'ArtisanBuild\\WaitingList\\WaitingListProvider', '--force' => true]);
+        $this->call('vendor:publish', ['--provider' => 'ArtisanBuild\\WaitingList\\WaitingListProvider', '--force' => true]);
         $this->info('Installation finished');
     }
 }
