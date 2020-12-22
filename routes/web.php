2619643462cafa132bf3623ccf7761f6fec0271b
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-12-21 19:43:53 -0600

    WIP

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use ArtisanBuild\WaitingList\Controllers\JoinWaitingList;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/join_waiting_list', JoinWaitingList::class)->name('waiting_list__join');
-Route::view('/joined_waiting_list', 'waiting_list::joined')->name('waiting_list__joined');
+Route::view('/joined_waiting_list', 'waiting::joined')->name('waiting_list__joined');
 Route::view('/join_waiting_list_form', 'waiting::join_form')->name('waiting_list__form');
